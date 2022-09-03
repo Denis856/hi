e3aceab6018ba024c613a0524318a01e819dd718
Denis856 <102806191+Denis856@users.noreply.github.com>
2022-09-03 14:33:41 +0500

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,19 +1,3 @@
-var header = document.querySelector('.header');
-
-header.style.background = "#451818";
-header.style.padding = "20px 0";
-
-var header_rows = document.querySelector('.header_rows');
-
-header_rows.style.display = "flex";
-header_rows.style.alignItems = "center";
-header_rows.style.justifyContent = "space-between";
-
-var head_list = document.querySelector('.head_list');
-
-head_list.style.display = "flex";
-head_list.style.alignItems = "center";
-
 
 var arrow = document.getElementById('arrow');
 var arrow2 = document.getElementById('arrow2');
@@ -29,3 +13,56 @@ function TurnArrow2() {
    block5_text0.classList.remove('show');
 }
 
+
+
+var arrow3 = document.getElementById('arrow3');
+var arrow2 = document.getElementById('arrow2');
+var block5_text01 = document.getElementById('block5_text01');
+
+function TurnArrow3() {
+   arrow3.classList.add('turn1');
+   block5_text01.classList.add('show');
+}
+
+function TurnArrow4() {
+   arrow3.classList.remove('turn1');
+   block5_text01.classList.remove('show');
+}
+
+
+var arrow5 = document.getElementById('arrow5');
+var arrow6 = document.getElementById('arrow6');
+var block5_text02 = document.getElementById('block5_text02');
+
+function TurnArrow5() {
+   arrow5.classList.add('turn1');
+   block5_text02.classList.add('show');
+}
+
+function TurnArrow6() {
+   arrow5.classList.remove('turn1');
+   block5_text02.classList.remove('show');
+}
+
+
+var arrow7 = document.getElementById('arrow7');
+var arrow8 = document.getElementById('arrow8');
+var block5_text03 = document.getElementById('block5_text03');
+
+function TurnArrow7() {
+   arrow7.classList.add('turn1');
+   block5_text03.classList.add('show');
+}
+
+function TurnArrow8() {
+   arrow7.classList.remove('turn1');
+   block5_text03.classList.remove('show');
+}
+
+
+$(document).ready(function(){
+   $('.menu_burger').click(function(event) {
+      $('.menu_burger,.nav').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
+});
